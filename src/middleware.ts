6d48bc83ccd17482de76e0689dd97daf22adf454
Abdul Kader <abdulkadernow@gmail.com>
2023-03-20 13:55:15 +0600

    TEst

---
+++ src/middleware.ts
@@ -0,0 +1,23 @@
+import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
+
+export async function middleware(req: NextRequest, ev: NextFetchEvent) {
+  const slug = req.nextUrl.pathname.split('/').pop();
+
+  if (!slug) {
+    return NextResponse.next();
+  }
+
+  const slugFetch = await fetch(`${req.nextUrl.origin}/api/get-url/${slug}`);
+  if (slugFetch.status === 404) {
+    return NextResponse.redirect(req.nextUrl.origin);
+  }
+  const data = await slugFetch.json();
+
+  if (data?.url) {
+    return NextResponse.redirect(data.url);
+  }
+}
+
+export const config = {
+  matcher: '/((?!api|_next/static|_next/image|favicon.ico).*)',
+};
