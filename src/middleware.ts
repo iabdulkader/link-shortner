5e24ed33158d405a3a8e0045309b96eff557e36e
Abdul Kader <abdulkadernow@gmail.com>
2023-03-20 16:43:25 +0600

    Test

--- src/middleware.ts
+++ src/middleware.ts
@@ -19,5 +19,5 @@ export async function middleware(req: NextRequest, ev: NextFetchEvent) {
 }
 
 export const config = {
-  matcher: '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  matcher: '/(.*)',
 };
